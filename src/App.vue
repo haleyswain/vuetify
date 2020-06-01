0931b57ae751017f98acdf87edf6ca8155c18a10
Haley Swain <haley.swain@pollinate.com>
2020-05-31 21:47:34 -0700

    update projects, add resources

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,14 @@
             <v-list-tile-title>Projects</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-list-tile to="/resources">
+          <v-list-tile-action>
+            <v-icon>bookmark</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Resources</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
@@ -65,7 +73,7 @@ export default {
   data () {
     return {
       clipped: false,
-      drawer: true,
+      drawer: false,
       fixed: false,
       miniVariant: false,
       right: true,
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,66 +1,105 @@
 <template>
   <v-layout>
     <v-flex class="profile-projects">
-      <v-card class="profile-projects__jbl">
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">JBL Custom Speakers</h3>
-            <div>Custom frontend for custom speakers!</div>
-          </div>
-        </v-card-title>
+       <v-card
+        class="mx-auto profile-projects__card"
+        width="400"
+      >
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          src="https://www.jbl.com/on/demandware.static/-/Sites-JB-US-Library/default/dw6659dbc4/glp/personalize/images/hero.jpg"
+        >
+        </v-img>
+        <v-card-title>JBL Custom Speakers</v-card-title>
+
+        <v-card-text class="text--primary">
+          <div>Custom frontend for custom speakers!</div>
+        </v-card-text>
 
         <v-card-actions>
           <v-btn flat color="green" href="https://www.jbl.com/bluetooth-speakers/JBLFLIP5WHTAMCSTM.html?iscustomizable=true">Explore</v-btn>
         </v-card-actions>
-        <img
-          class="profile-projects__jbl-image"
-          src="https://www.jbl.com/on/demandware.static/-/Sites-JB-US-Library/default/dw6659dbc4/glp/personalize/images/hero.jpg"
-        >
       </v-card>
-      <v-card class="profile-projects__snake">
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Snake</h3>
-            <div>Programming exercise recreating the classic arcade game</div>
-          </div>
-        </v-card-title>
+      <v-card
+        class="mx-auto profile-projects__card"
+        width="400"
+      >
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src='require("../assets/snake.png")'
+        >
+        </v-img>
+        <v-card-title>Snake</v-card-title>
+
+        <v-card-text class="text--primary">
+          <div>Programming exercise recreating the classic arcade game</div>
+        </v-card-text>
 
         <v-card-actions>
           <v-btn flat color="green" href="https://github.com/haleyswain/snake">Explore</v-btn>
         </v-card-actions>
-        <img
-          class="profile-projects__snake-image"
-          src="../assets/snake.png"
-        >
-
       </v-card>
 
-      <v-card class="profile-projects__energytrust">
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">Energy Trust of Oregon Forms</h3>
-            <div>Redesign and maintain all ETO forms.</div>
-          </div>
-        </v-card-title>
+      <v-card
+        class="mx-auto profile-projects__card"
+        width="400"
+      >
+        <v-img
+          class="white--text align-end profile-projects__energytrust"
+          height="200px"
+          :contain="true"
+          :src='require("../assets/energy-trust.png")'
+        >
+        </v-img>
+        <v-card-title>Energy Trust of Oregon Forms</v-card-title>
+
+        <v-card-text class="text--primary">
+          <div>Redesign and maintain all ETO forms.</div>
+        </v-card-text>
 
         <v-card-actions>
           <v-btn flat color="green" href="https://forms.energytrust.org/esaverkits">Explore</v-btn>
         </v-card-actions>
-        <img
-          class="profile-projects__energytrust-image"
-          src="../assets/energy-trust.png"
+      </v-card>
+
+      <v-card
+        class="mx-auto profile-projects__card"
+        width="400"
+      >
+        <v-img
+          class="white--text align-end profile-projects__UA"
+          height="200px"
+          :contain="true"
+          :src='require("../assets/UAlogo.png")'
         >
+        </v-img>
+        <v-card-title>Under Armour Team Uniform Configurator</v-card-title>
+
+        <v-card-text class="text--primary">
+          <div>Maintenance and new feature builds for Under Armour's internal 3D uniform configurator</div>
+        </v-card-text>
       </v-card>
     </v-flex>
   </v-layout>
 </template>
 
-<script></script>
+<script>
+
+</script>
 <style lang="scss" scoped>
 .profile-projects {
   -ms-flex-positive: 0;
   display: flex;
   flex-direction: row;
+  margin-top: 20px;
+  flex-wrap: wrap;
+
+  &__card {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
   &__snake {
     width: 500px;
     height: 50%;
@@ -71,8 +110,6 @@
       }
     }
     &__energytrust {
-      width: 500px;
-      height: 50%;
 
      &-image {
         width: 100%;
@@ -82,7 +119,7 @@
     }
     &__jbl {
       width: 500px;
-      height: 80%;
+      height: 50%;
      &-image {
         width: 100%;
         bottom: 0px;
